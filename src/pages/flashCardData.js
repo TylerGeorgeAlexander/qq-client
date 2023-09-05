27848f9db600d63d88a081552a05fdf1e2b829e6
Tyler Alexander <TylerGeorgeAlexander@gmail.com>
2023-09-05 10:57:17 -0400

    Chore: dummy data

---
+++ src/pages/flashCardData.js
@@ -0,0 +1,19 @@
+// flashCardData.js
+const flashCardData = [
+    {
+      title: "Math Flash Card",
+      query: "What is 2 + 2?",
+      assertion: "4",
+      timestamp: "2023-09-05",
+    },
+    {
+      title: "Science Flash Card",
+      query: "What is the capital of France?",
+      assertion: "Paris",
+      timestamp: "2023-09-05",
+    },
+    // Add more flash card data entries...
+  ];
+  
+  export default flashCardData;
+  
