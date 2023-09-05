c792ef727d40991eb5b2410ae8eaff7c44dc2619
Tyler Alexander <TylerGeorgeAlexander@gmail.com>
2023-09-05 12:06:20 -0400

    chore: dummy data updated with searchId

--- src/pages/flashCardData.js
+++ src/pages/flashCardData.js
@@ -1,12 +1,14 @@
 // flashCardData.js
 const flashCardData = [
     {
+      searchId: "search-id-1",
       title: "Math Flash Card",
       query: "What is 2 + 2?",
       assertion: "4",
       timestamp: "2023-09-05",
     },
     {
+      searchId: "search-id-2",
       title: "Science Flash Card",
       query: "What is the capital of France?",
       assertion: "Paris",
