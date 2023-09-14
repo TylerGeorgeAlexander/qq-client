686ccb3b60dd8fe4b932ac59d799ad9986e79fff
Tyler Alexander <TylerGeorgeAlexander@gmail.com>
2023-09-14 10:35:43 -0400

    refactor

--- server.js
+++ server.js
@@ -1,24 +1,17 @@
-import express from 'express';
-import { fileURLToPath } from 'url';
-import { join, dirname } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+const express = require('express');
+const path = require('path');
 const app = express();
 
 // Serve static assets (e.g., your built React app)
-app.use(express.static(join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 // Define a catch-all route
 app.get('*', (req, res) => {
-  res.sendFile(join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 // Start the server
-const { PORT = 3000 } = import.meta.env;
-const port = Number(PORT);
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
